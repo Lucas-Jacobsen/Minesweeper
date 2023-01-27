fab5a5a407ecc31e8328a2d04c672476e9c36e30
Lucas Jacobsen <lukejacobsen33@gmail.com>
2023-01-27 08:07:48 -0700

    Add project files.

---
+++ Milestone350/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Milestone350.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
