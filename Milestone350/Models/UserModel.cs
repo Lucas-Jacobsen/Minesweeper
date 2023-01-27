491249bfbd0cc1e7f372378b44767f7a122e1a7c
CFirnkoess <Truly@LAPTOP-73S7HVL7>
2023-01-27 14:05:55 -0700

    Test Final project

---
+++ Milestone350/Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Milestone350.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public  string Password { get; set; }
+
+
+    }
+
+}
